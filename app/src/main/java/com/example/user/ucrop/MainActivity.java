0b1915119787893db4c39cb5158ffbc6d567d472
Kotas Kott <kott.kotas@gmail.com>
2017-05-11 14:22:16 +0300

    HomeWork
    ScrollView Added, SeekBar + TextView with Percentage from SeekBar

--- app/src/main/java/com/example/user/ucrop/MainActivity.java
+++ app/src/main/java/com/example/user/ucrop/MainActivity.java
@@ -6,12 +6,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etCoordX;
     EditText etCoordY;
+    TextView tvPercentage;
 
 
 
@@ -28,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         RadioButton rbImageSource = (RadioButton) findViewById(R.id.rbImageSource);
         etCoordX = (EditText) findViewById(R.id.etCoordX);
         etCoordY = (EditText) findViewById(R.id.etCoordY);
+        tvPercentage = (TextView) findViewById(R.id.tvPercentage);
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+
 
 
 
@@ -70,6 +76,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tvPercentage.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 
 
